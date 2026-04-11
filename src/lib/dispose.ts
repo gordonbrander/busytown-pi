9277a58939665aeb87274d9b76af6bc299b5b5cd
Gordon Brander <gordonbrander@users.noreply.github.com>
2026-04-11 17:28:40 +0300

    Remove dead file

--- src/lib/dispose.ts
+++
@@ -1,8 +0,0 @@
-export type AsyncDisposable = {
-  [Symbol.asyncDispose](): Promise<void>;
-};
-
-/** Call the async dispose method of a disposable object. */
-export const asyncDispose = (disposable: AsyncDisposable): Promise<void> => {
-  return disposable[Symbol.asyncDispose]();
-};
